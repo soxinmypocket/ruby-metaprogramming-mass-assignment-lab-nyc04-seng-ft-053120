c47a5423cb28f15bb0187306baea57fcdf9c27bd
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-06-29 16:13:04 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  #your code here
+  attr_accessor :name, :hair_color, :birthday, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes = nil) #default argument
+    if attributes.each do |key, value| #accepts a hash
+      self.send("#{key}=", value) #push instance to self
+        end
+      end
+    end
 end
